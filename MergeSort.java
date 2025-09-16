f2f6129bac4361908cc76ec5f53c63ed9e27b4ef
JoshYod <joshuadayne@icloud.com>
2025-09-16 18:21:39 +0000

    create mergesort file

---
+++ MergeSort.java
@@ -0,0 +1,13 @@
+public class MergeSort {
+    public static void main(String[] args){
+        //case1
+        int [] array1 = {7,6,4,5,2,3};
+        Systen.out.Println("\n Before Merge Sorting")
+        for (int number : array1){
+                System.out.print(number + ", ");
+        }//end for
+
+    }//end main
+
+
+}//end class
--- makefile
+++ makefile
@@ -2,3 +2,9 @@ save:
 	git add *
 	git commit *
 	git push origin main
+
+runMerge: MergeSort.class
+	java MergeSort
+
+MergeSort.class:
+	javac MergeSort
