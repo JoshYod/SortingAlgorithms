3e8f545efed0a761ca0bab77103f2c596d611cb2
JoshYod <joshuadayne@icloud.com>
2025-09-16 19:12:03 +0000

    case 3 and 2 merge sort

--- MergeSort.java
+++ MergeSort.java
@@ -1,18 +1,44 @@
+import java.util.*;
+
 public class MergeSort {
     public static void main(String[] args){
         //case1
         int [] array1 = {7,6,4,5,2,3};
-        System.out.print("\nBefore Merge Sorting: ");
+        System.out.print("\nBefore Merge Sorting Case 1: ");
         for (int number : array1){
                 System.out.print(number + ", ");
         }//end for
         int lowIndex = 0,highIndex=array1.length-1;
         array1 = mergeSort(array1, lowIndex, highIndex);
-        System.out.print("\nAfter Merge Sorting: ");
+        System.out.print("\nAfter Merge Sorting Case 1: ");
         for(int number : array1){
             System.out.print(number + ", ");
         }//end for
 
+        //case 2
+        Random rand = new Random();
+        int array2 [] = new int[1000000];
+        for (int i = 0; i<array2.length; i++){
+            array2[i] = rand.nextInt(1000000);
+        }//end for
+        long startTime1 = System.nanoTime();
+        array2=mergeSort(array2, lowIndex, array2.length-1);
+        long endTime1 = System.nanoTime();
+        long totalTime1 = endTime1 - startTime1;
+        System.out.print("\nAfter Merge Sorting for case 2 time total: ");
+        System.out.print(totalTime1/1000000000.0 + " seconds");
+
+    //case 3
+    /*int array3[] = new int [1000000];
+    for(int i = 0; i < array3.length-1; i++){
+        array3 [i] = i;
+        long startTime2 = System.nanoTime();
+        array3 =mergeSort(array3, lowIndex, array3.length-1);
+        long endTime2 = System.nanoTime();
+        long totalTime2 = endTime2 - startTime2;
+        System.out.print("\nAfter Merge Sorting for case 3 time total: ");
+        System.out.print(totalTime2/1000000000.0 + " seconds");
+    }//end for*/
 
         System.out.println();
     }//end main
